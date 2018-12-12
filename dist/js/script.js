ea92a4052786a90c5c68e73cb542f049aa350acf
Andrii <andriibozhko1@gmail.com>
2018-12-12 17:33:38 +0200

    done

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,97 +1,109 @@
-(function () {
-  const listItems = ['Apple', 'Lemon', 'Lime', 'Orange', 'Strawberry'];
-  const dropDown = document.querySelector('.DropDown');
-  const inputText = document.querySelector('.app__input');
-  const app = document.querySelector('.app');
-  const selectedItems = document.querySelector('.selected');
+(function() {
+  const listItems = ["Apple", "Lemon", "Lime", "Orange", "Strawberry"];
+  const dropDown = document.querySelector(".DropDown");
+  const inputText = document.querySelector(".app__input");
+  const app = document.querySelector(".app");
+  const selectedItems = document.querySelector(".selected");
+  let counter;
 
-  const fillObjItems = function () {
-    const list = [];
-    for (let i = 0; i < listItems.length; i++) {
-      const itemsObj = {
-        id: i + 1,
-        value: listItems[i],
-      };
-      list.push(itemsObj);
-    }
-
-    inputText.addEventListener('keyup', () => {
-      const inputValue = inputText.value;
-      filterList(list, inputValue);
-    });
-
-    inputText.addEventListener('click', () => {
-      renderList(list);
-    });
-  };
-
-  const renderList = function (list) {
-    dropDown.innerHTML = '';
-    const createList = document.createElement('ul');
-    createList.classList.add('DropDown__list');
+  const renderItems = function(list) {
+    counter = -1;
+    const createList = document.createElement("ul");
+    createList.classList.add("DropDown__list");
+    dropDown.innerHTML = "";
     dropDown.appendChild(createList);
 
     for (let i = 0; i < list.length; i++) {
-      const createListItems = document.createElement('li');
-      createListItems.classList.add('DropDown__items');
-      createListItems.dataset.value = list[i].value;
-      createListItems.innerHTML = list[i].value;
+      const createListItems = document.createElement("li");
+      createListItems.classList.add("DropDown__items");
+      createListItems.dataset.value = list[i];
+      createListItems.innerHTML = list[i];
       createList.appendChild(createListItems);
     }
   };
 
-  const filterList = function (list, inputValue) {
-    const findItems = inputValue.charAt(0).toUpperCase() + inputValue.slice(1);
-    const filteredList = [];
-    for (let i = 0; i < list.length; i++) {
-      if (list[i].value.includes(findItems)) {
-        filteredList.push(list[i]);
+  inputText.addEventListener('keydown', function(e) {
+    const DropDownItems = document.querySelectorAll('.DropDown__items');
+    let tempItems;
+    
+    if(e.keyCode === 40) {
+       if(counter >= DropDownItems.length - 1){
+         counter = - 1;
+       }
+       tempItems = counter;
+       if(tempItems === -1) {
+        tempItems = DropDownItems.length - 1;
+       }
+       counter++;
+       DropDownItems[counter].classList.add('test');
+       DropDownItems[tempItems].classList.remove('test');
+       inputText.value = DropDownItems[counter].dataset.value;
+    } else if(e.keyCode === 38) {
+      if(counter <= 0){
+        counter = DropDownItems.length;
+      }
+      tempItems = counter;
+      if(tempItems === DropDownItems.length) {
+        tempItems = 0;
+      }
+      counter--;
+      DropDownItems[counter].classList.add('test');
+      DropDownItems[tempItems].classList.remove('test');
+      inputText.value = DropDownItems[counter].dataset.value;
+    } else if(e.keyCode === 13) {
+      createSelectedBlocks(inputText.value);
+    }
+  })
+
+  const filterList = function(arr, inputValue) {
+    const filteredValue =
+      inputValue.charAt(0).toUpperCase() + inputValue.slice(1);
+    const filteredItems = [];
+
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i].includes(filteredValue)) {
+        filteredItems.push(arr[i]);
       }
     }
-    renderList(filteredList);
+    renderItems(filteredItems);
   };
 
-  const createSelectedBlocks = function (text) {
-    const selectedBlocks = document.createElement('span');
-    selectedBlocks.classList.add('selected__items');
+  const createSelectedBlocks = function(text) {
+    const selectedBlocks = document.createElement("span");
+    selectedBlocks.classList.add("selected__items");
     selectedBlocks.innerHTML = text;
 
-    const crossIcon = document.createElement('span');
-    crossIcon.classList.add('selected__crossIcon');
+    const crossIcon = document.createElement("span");
+    crossIcon.classList.add("selected__crossIcon");
 
     selectedBlocks.appendChild(crossIcon);
     selectedItems.appendChild(selectedBlocks);
 
-    inputText.value = '';
-    dropDown.innerHTML = '';
+    inputText.value = "";
+    dropDown.innerHTML = "";
   };
 
-
-  document.body.addEventListener('click', (e) => {
-    if (e.target.className !== "app__input" && e.target.className !== "DropDown__items") {
-      dropDown.innerHTML = "";
-    }
+  inputText.addEventListener("click", function() {
+    renderItems(listItems);
   });
 
-  app.addEventListener('click', (e) => {
-    if(e.target.className === 'DropDown__items') {
+  inputText.addEventListener("keyup", function(e) {
+      if(e.keyCode !== 38 && e.keyCode !== 40 && e.keyCode !== 13) {
+        filterList(listItems, inputText.value);
+      }
+  });
+
+  document.body.addEventListener("click", (e) => {
+    if (e.target.className !== "app__input" && e.target.className !== "DropDown__items"){
+      dropDown.innerHTML = "";
+    }else if(e.target.className === "DropDown__items") {
       createSelectedBlocks(e.target.dataset.value);
     }
   });
-
-  app.addEventListener('click', (e) => {
-    if(e.target.className === 'selected__crossIcon') {
+  
+  app.addEventListener('click', function(e) {
+    if(e.target.classList == 'selected__crossIcon') {
       e.target.parentElement.remove();
     }
   });
-
-  inputText.addEventListener('keydown', (e) => {
-    if(e.keyCode === 13) {
-      if(inputText.value.trim() !== '') {
-        createSelectedBlocks(inputText.value);
-      }
-    }
-  });
-
-  fillObjItems();
-}());
+})();
